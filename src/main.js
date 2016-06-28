0b4df21cd9a6a5ec5f5d0ae1355b307f0a5c5e3e
Federico Minzoni <fminzoni@enter.it>
2016-06-28 15:45:34 +0200

    init repo

---
+++ src/main.js
@@ -0,0 +1,35 @@
+
+const PATH = require('path');
+const express = require('express');
+const horizon = require('@horizon/server');
+
+const app = express();
+const ROOT = '.';
+
+const pageRouter = express.Router();
+
+pageRouter.get('*', (req, res, next) => {
+    console.log(req.url);
+    if(req.url.indexOf('/public') < 0) {
+        req.url = '/public/index.html';    
+    }
+    next();
+});
+
+app.use('/', pageRouter);
+
+app.use('/public', express.static(PATH.resolve(__dirname, ROOT, 'public')));
+
+
+const http_server = app.listen(8181);
+const options = {
+    project_name: 'simpleapp',
+    auth: {       
+      token_secret: 'n36wVGoUE/4gJGY9vDS3+rhkaFQByf9XmsWEqrdQcIXAvUqzjgbq6eJz7MMBd51Q/GYT6PtFxWP+JYy8J4m06w==',
+      allow_anonymous: true,
+      allow_unauthenticated: true,
+    },
+   };
+const horizon_server = horizon(http_server, options);
+
+console.log('Listening on port 8181.');
