33fa155b5f9b6e870f9a5e9a49e59f3b2c2bd771
Federico Minzoni <fminzoni@enter.it>
2016-07-15 02:27:11 +0200

    add simple chat code

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # horizon-docker-simpleapp
 
 An example application based on **[Horizon.io](http://horizon.io/)**. Horizon is a realtime, open-source backend for JavaScript apps.
+
+## Usage
+
+**Dev Mode**
+
+docker-compose -f docker-compose.dev.yml up -d
+
+**Debug Mode**
+
+1. docker-compose -f docker-compose.dev.yml up -d rethinkdb  
+2. npm install -g horizon  
+3. cd src  
+4. npm install  
+5. node main.js or launch Visual Studio Code debugger 
+
+**Force Import the RethinkDB schema**
+
+hz set-schema --connect localhost:28015 schema.toml
+
+
--- src/public/app.js
+++ src/public/app.js
@@ -1,25 +1,65 @@
 const horizon = new Horizon()
+const messages = horizon('messages')
 horizon.connect()
 
 const app = new Vue({
   el: '#app',
   template: `
-    <div> {{ message }} </div>
+    <div>
+      <div> Status: {{ status }} </div>
+      <div id="chatMessages">
+        <ul>
+          <li v-for="message in messages">
+            {{ message.text }}
+          </li>
+        </ul>
+      </div>
+      <div id="input">
+        <input @keyup.enter="sendMessage" ></input>
+      </div>
+    </div>
   `,
   data: {
-    message: ''
+    status: '',
+    // Our dynamic list of chat messages
+    messages: []
   },
   created() {
-   
+    // Subscribe to messages
+    messages.order('datetime', 'descending').limit(10).watch()
+    .subscribe(allMessages => {
+        // Make a copy of the array and reverse it, so newest images push into
+        // the messages feed from the bottom of the rendered list. (Otherwise
+        // they appear initially at the top and move down)
+        this.messages = [...allMessages].reverse()
+      },
+      // When error occurs on server
+      error => console.log(error)
+    )
+
     // Triggers when client successfully connects to server
     horizon.onReady().subscribe(
-    () => { this.message = 'Connected to Horizon server' }
+    () => { this.status = 'Connected to Horizon server' }
     )
 
     // Triggers when disconnected from server
     horizon.onDisconnected().subscribe(
-      () => { this.message = 'Disconnected from Horizon server' }
+      () => { this.status = 'Disconnected from Horizon server' }
     )
   },
-
+  methods: {
+    sendMessage(event) {
+      messages.store({
+        text: event.target.value, // Current value inside <input> tag
+        datetime: new Date() // Warning clock skew!
+      }).subscribe(
+          // Returns id of saved objects
+          result => console.log(result),
+          // Returns server error message
+          error => console.log(error)
+        )
+        // Clear input for next message
+        event.target.value = ''
+    }
+  }
 })
